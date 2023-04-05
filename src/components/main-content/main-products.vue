e1ad953199cfbd6694dd63bce91efe4cc51f5a48
ilshat2001 <ilshat2001hus@mail.ru>
2023-04-05 17:04:17 +0300

    renteyk2

--- src/components/main-content/main-products.vue
+++ src/components/main-content/main-products.vue
@@ -4,10 +4,31 @@
   </div>
 
   <div class="products-images">
-    <div class="products-breads"></div>
-    <div class="products-pies"></div>
-    <div class="products-conditers"></div>
-    <div class="products-gateau"></div>
+    <div class="product">
+      <div class="product-icon">
+        <div class="products-breads"></div>
+      </div>
+      <h3>Хлеба</h3>
+    </div>
+    <div class="product">
+      <div class="product-icon">
+        <div class="products-pies"></div>
+      </div>
+      <h3>Выпечка</h3>
+    </div>
+    <div class="product">
+      <div class="product-icon">
+        <div class="products-conditers"></div>
+      </div>
+      <h3>Кондитерские изделия</h3>
+    </div>
+    <div class="product">
+      <div class="product-icon">
+        <div class="products-gateau"></div>
+      </div>
+      <h3>Пироги</h3>
+    </div>
+
   </div>
 </template>
 
@@ -21,25 +42,49 @@ export default {
 .main-products
   display: flex
   padding-top: 100px
+  color: $ren
 
 .products-images
   display: flex
+  justify-content: space-between
+  padding-top: 50px
+  width: 100%
+  color: $ren
+  .product
+    display: flex
+    flex-direction: column
+    align-items: center
+    width: 100%
+    text-align: center
+    gap: 30px
+    .product-icon
+      width: 230px
+      height: 230px
+      padding: 40px
+      +border-radius(999px)
+      border: 3px solid $ren
+      &:hover
+        background-color: rgba(254, 194, 99, 0.1)
+      > div
+        +background-image-settings()
+        background-size: contain
+        width: 100%
+        height: 100%
 
-  .products-breads
-    background-image: url("/src/assets/images/icons/loaf.png")
-    +background-image-settings()
-    width: 100px
-    height: 100px
 
-.products-pies
-  background-image: url("/src/assets/images/icons/pies.png")
-  +background-image-settings()
-  width: 80px
-  height: 80px
-.products-conditers
-  background-image: url("/src/assets/images/icons/conditer.png")
-  +background-image-settings()
-  width: 80px
-  height: 80px
+
+        &.products-breads
+          background-image: url("/src/assets/images/icons/loaf.png")
+
+
+        &.products-pies
+          background-image: url("/src/assets/images/icons/pies.png")
+
+        &.products-conditers
+          background-image: url("/src/assets/images/icons/cake.png")
+
+        &.products-gateau
+          background-image: url("/src/assets/images/icons/pie.png")
+
 
 </style>
