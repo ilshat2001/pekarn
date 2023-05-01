e82be5ca2b178cfe47413a55333b01ff64c20188
ilshat2001 <ilshat2001hus@mail.ru>
2023-05-01 17:22:57 +0300

    add content about

--- src/components/pekarn-about.vue
+++ src/components/pekarn-about.vue
@@ -1,8 +1,16 @@
 <template>
 <div class="about-page">
-<h3>
+  <div class="about-text">
+    <h1>О нас</h1>
+    <h3>«Дом хлеба и домашней выпечки» - это место, где всегда можно вкусно провести время.  </h3>
+    <h3>У нас большой опыт в пищевом производстве. Мы успели много: разработали и запустили в производство более 200 видов разнообразной и качественной продукции — сытные и сладкие пироги, фирменную выпечку, хлеба, десерты и торты.</h3></div>
+  <div class="about-contacts">
+    <h1>Контакты</h1>
+    <h3>Казань, 2-я Староаракчинская 41</h3>
+    <div class="about-location">
 
-</h3>
+    </div>
+  </div>
 </div>
 </template>
 
@@ -23,4 +31,12 @@ export default {
   flex-direction: column
   align-items: center
   padding: 200px 50px 0
+.about-text
+  width: 50%
+.about-contacts
+  width: 100%
+  height: 100%
+.about-location
+  background-image: url("/src/assets/images/location.png")
+  +background-image-settings()
 </style>
