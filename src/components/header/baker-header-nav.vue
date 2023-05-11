397c3d070a5a498fae430df073dcd263ca899118
ilshat2001 <ilshat2001hus@mail.ru>
2023-05-11 21:26:24 +0300

    add burger

--- src/components/header/baker-header-nav.vue
+++ src/components/header/baker-header-nav.vue
@@ -1,5 +1,8 @@
 <template>
   <nav>
+    <div class="burger">
+      <span></span>
+    </div>
     <ul class="header-nav">
       <li class="header-nav-item">
         <h5>
@@ -50,7 +53,10 @@
 
 <script>
 export default {
-  name: "bakerHeaderNav"
+  name: "bakerHeaderNav",
+  mounted() {
+    
+  }
 }
 </script>
 
@@ -64,16 +70,15 @@ export default {
     width: 100%
     max-width: 900px
     z-index: 2
-
-    @media (max-width: 1080px)  // phone
-      display: none
-
-    @media (min-width: 1081px)  // desktop
-      display: flex
-
     .header-nav
-      display: flex
       gap: 50px
+      @media (min-width: $desktopMin) // Desktop
+        display: flex
+      @media (max-width: $tabletMax) and (min-width: $tabletMin)  // Table
+        display: none
+      @media (max-width: $mobileMax) // Mobile
+        display: none
+
 
       .header-nav-item
         hr
@@ -92,4 +97,44 @@ export default {
       height: 100px
       background-image: url("/src/assets/images/logo.png")
       +background-image-settings()
+.burger
+  display: flex
+  position: relative
+  z-index: 50
+  align-items: center
+  justify-content: flex-end
+  width: 40px
+  height: 24px
+  @media (min-width: $desktopMin) // Desktop
+    display: none
+  @media (max-width: $tabletMax) and (min-width: $tabletMin)  // Table
+    display: flex
+  @media (max-width: $mobileMax) // Mobile
+    display: flex
+.burger span
+  height: 2px
+  width: 100%
+  transform: scale(1)
+  background-color: white
+.burger::before, .burger::after
+  content: ' '
+  position: absolute
+  height: 2px
+  width: 100%
+  background-color: white
+  transition: all 0.3s ease 0s
+
+.burger::before
+  top: 0
+.burger::after
+  bottom: 0
+
+.burger.active span
+  transform: scale(0)
+.burger.active::before
+  top: 50%
+  transform: rotate(-45deg) translate(0, -50%)
+.burger.active::after
+  bottom: 50%
+  transform: rotate(45deg) translate(0, 50%)
 </style>
--- src/components/pekarn-about.vue
+++ src/components/pekarn-about.vue
@@ -1,14 +1,30 @@
 <template>
 <div class="about-page">
+  <section>
+  <div class="about-image"></div>
   <div class="about-text">
     <h1>О нас</h1>
     <h3>«Дом хлеба и домашней выпечки» - это место, где всегда можно вкусно провести время.  </h3>
-    <h3>У нас большой опыт в пищевом производстве. Мы успели много: разработали и запустили в производство более 200 видов разнообразной и качественной продукции — сытные и сладкие пироги, фирменную выпечку, хлеба, десерты и торты.</h3></div>
+    <h3>У нас большой опыт в пищевом производстве. Мы успели много: разработали и запустили в производство более 200 видов разнообразной и качественной продукции — сытные и сладкие пироги, фирменную выпечку, хлеба, десерты и торты.</h3>
+  </div>
+  </section>
   <div class="about-contacts">
     <h1>Контакты</h1>
-    <h3>Казань, 2-я Староаракчинская 41</h3>
     <div class="about-location">
-
+      <div class="about-location-logo">
+      </div>
+      <h3>Казань, Старо-Аракчинская 2-я, 41</h3>
+    </div>
+    <div class="about-number">
+      <div class="about-number-logo"></div>
+      <h3><a href="tel:+78435148214">+7 (843) 514‒82‒14</a></h3>
+    </div>
+      <div class="about-number">
+      <div class="about-number-logo"></div>
+      <h3><a href="tel:+79179114989">+7‒917‒911‒49‒89</a></h3>
+    </div>
+    <div class="map">
+      <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A53b74b4d57a9b9865bcd1a933418312c0d130413525b6a3f4ff9a7b2d8abff65&amp;source=constructor" width="99%" height="600" frameborder="0"></iframe>
     </div>
   </div>
 </div>
@@ -28,15 +44,48 @@ export default {
   min-height: 150vh
   position: relative
   display: flex
-  flex-direction: column
   align-items: center
   padding: 200px 50px 0
+  flex-direction: column
+  gap: 30px
 .about-text
-  width: 50%
+  width: 70%
+  color: $ren
 .about-contacts
-  width: 100%
+  width: 70%
   height: 100%
-.about-location
+  display: flex
+  flex-direction: column
+  gap: 30px
+  align-items: center
+  color: $ren
+  +border-radius(20px)
+  border: 4px solid $ren
+.about-location-logo
   background-image: url("/src/assets/images/location.png")
   +background-image-settings()
+  width: 40px
+  height: 40px
+.about-location
+  display: flex
+.about-number-logo
+  background-image: url("/src/assets/images/phone.png")
+  +background-image-settings()
+  width: 32px
+  height: 32px
+.about-number
+  display: flex
+.about-image
+  background-image: url("/src/assets/images/room.jpeg")
+  +background-image-settings()
+  width: 500px
+  height: 500px
+section
+  display: flex
+  width: 100%
+  justify-content: space-between
+.map
+  display: flex
+  justify-content: center
+  padding-bottom: 10px
 </style>
